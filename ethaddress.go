f5f293a41ab98a1655609a893bff6a272ae9ce79
steven004 <steven004@gmail.com>
2023-07-13 14:02:06 +0800

    Add parameters

--- ethaddress.go
+++ ethaddress.go
@@ -7,6 +7,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -16,28 +17,57 @@ import (
 )
 
 func main() {
-	// mnemonic := "atom favorite rely funny disorder vast echo spin segment market cat hood"
-	// Get user input for the mnemonic
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your mnemonic: ")
-	mnemonic, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
+	var mnemonic, indexStr string
+	var index uint64
+	var err error
+
+	// args: memonic and path string
+	args := os.Args[1:]
+	if len(args) == 2 {
+		mnemonic = args[0]
+		indexStr = args[1]
+
+	} else {
+		// mnemonic := "atom favorite rely funny disorder vast echo spin segment market cat hood"
+		// Get user input for the mnemonic
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print("Enter your mnemonic: ")
+		mnemonic, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading mnemonic:", err)
+			return
+
+		}
+		fmt.Print("Enter BIP32 index(m/44'/60'/0'/0/index): ")
+		indexStr, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading index:", err)
+			return
+		}
 	}
 
 	// clean up the mnemonic string
 	mnemonic = strings.TrimSpace(mnemonic)
+	indexStr = strings.TrimSpace(indexStr)
+
+	index, err = strconv.ParseUint(indexStr, 10, 32)
+	if err != nil {
+		fmt.Println("The index need to be an uint number", indexStr)
+		return
+	}
+
+	fmt.Println(mnemonic, indexStr, index)
 
 	seed := bip39.NewSeed(mnemonic, "")
-
 	masterKey, err := bip32.NewMasterKey(seed)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// Derive key for m/44'/60'/0'/0/0 path
+	// Derive key for m/44'/60'/0'/0/index path
 	childKey, err := masterKey.NewChildKey(bip32.FirstHardenedChild + 44)
 	if err != nil {
 		fmt.Println(err)
@@ -58,7 +88,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	childKey, err = childKey.NewChildKey(0)
+	childKey, err = childKey.NewChildKey(uint32(index))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -97,4 +127,6 @@ func main() {
 	// encode public key to hex string
 	hexPublicKey := hexutil.Encode(crypto.FromECDSAPub(publicKeyECDSA)[1:])
 	fmt.Println("Hex public key:", hexPublicKey)
+
+	fmt.Println("BIP32 path:", "m/44'/60'/0'/0/", index)
 }
