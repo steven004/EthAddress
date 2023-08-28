32df445f7dfd2f9d1f095ef4280575f95adbeb72
steven004 <steven004@gmail.com>
2023-08-28 17:17:36 +0800

    added flags and batch creation

--- ethaddress.go
+++ ethaddress.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/ecdsa"
 	"encoding/hex"
+	"flag"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -17,49 +14,27 @@ import (
 )
 
 func main() {
-	var mnemonic, indexStr string
-	var index uint64
-	var err error
+	mnemonicPtr := flag.String("m", "", "Specify the mnemonic string")
+	startIndexPtr := flag.Uint("f", 0, "Specify the first index (integer) of HD address, default:0")
+	endIndexPtr := flag.Uint("t", 0, "Specify the last index (integer) of HD address")
+	showHelp := flag.Bool("h", false, "Show usage information")
 
-	// args: memonic and path string
-	args := os.Args[1:]
-	if len(args) == 2 {
-		mnemonic = args[0]
-		indexStr = args[1]
+	var endIndex uint
 
-	} else {
-		// mnemonic := "atom favorite rely funny disorder vast echo spin segment market cat hood"
-		// Get user input for the mnemonic
-		reader := bufio.NewReader(os.Stdin)
+	flag.Parse()
 
-		fmt.Print("Enter your mnemonic: ")
-		mnemonic, err = reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading mnemonic:", err)
-			return
-
-		}
-		fmt.Print("Enter BIP32 index(m/44'/60'/0'/0/index): ")
-		indexStr, err = reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading index:", err)
-			return
-		}
-	}
-
-	// clean up the mnemonic string
-	mnemonic = strings.TrimSpace(mnemonic)
-	indexStr = strings.TrimSpace(indexStr)
-
-	index, err = strconv.ParseUint(indexStr, 10, 32)
-	if err != nil {
-		fmt.Println("The index need to be an uint number", indexStr)
+	if *showHelp || *mnemonicPtr == "" {
+		flag.Usage()
 		return
 	}
 
-	fmt.Println(mnemonic, indexStr, index)
+	if *endIndexPtr < *startIndexPtr {
+		endIndex = *startIndexPtr
+	} else {
+		endIndex = *endIndexPtr
+	}
 
-	seed := bip39.NewSeed(mnemonic, "")
+	seed := bip39.NewSeed(*mnemonicPtr, "")
 	masterKey, err := bip32.NewMasterKey(seed)
 
 	if err != nil {
@@ -88,45 +63,58 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	childKey, err = childKey.NewChildKey(uint32(index))
-	if err != nil {
-		fmt.Println(err)
-		return
+
+	for i := *startIndexPtr; i <= endIndex; i++ {
+		indexKey, err := childKey.NewChildKey(uint32(i))
+		if err != nil {
+			fmt.Println(err, "000000")
+			return
+		}
+
+		// Ugly fix of the EOF issue when read from a bytes reader
+		var privateKey *ecdsa.PrivateKey
+		for j := 0; j < 100; j++ {
+			reader := bytes.NewReader(indexKey.Key)
+			privateKey, err = ecdsa.GenerateKey(crypto.S256(), reader)
+			if err == nil {
+				break
+			}
+		}
+		// reader := bytes.NewReader(indexKey.Key)
+		// privateKey, err := ecdsa.GenerateKey(crypto.S256(), reader)
+		if err != nil {
+			fmt.Println(err, "100000")
+			return
+		}
+
+		publicKey := privateKey.Public()
+		publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+		if !ok {
+			fmt.Println("error casting public key to ECDSA")
+			return
+		}
+
+		fmt.Println("BIP32 path:", "m/44'/60'/0'/0/", i)
+
+		address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+		fmt.Println("Ethereum address:", address)
+
+		// // create a new keystore and add the key to it
+		// ks := keystore.NewKeyStore(".", keystore.StandardScryptN, keystore.StandardScryptP)
+		// newAcc, err := ks.ImportECDSA(privateKey, "")
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	return
+		// }
+
+		// fmt.Println("Keystore path:", ks.JoinPath(newAcc.Address.Hex()))
+
+		// encode private key to hex string
+		hexPrivateKey := hex.EncodeToString(crypto.FromECDSA(privateKey))
+		fmt.Println("Hex private key:", hexPrivateKey)
+
+		// encode public key to hex string
+		hexPublicKey := hexutil.Encode(crypto.FromECDSAPub(publicKeyECDSA)[1:])
+		fmt.Println("Hex public key:", hexPublicKey)
 	}
-
-	privateKey, err := ecdsa.GenerateKey(crypto.S256(), bytes.NewReader(childKey.Key))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		fmt.Println("error casting public key to ECDSA")
-		return
-	}
-
-	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
-	fmt.Println("Ethereum address:", address)
-
-	// // create a new keystore and add the key to it
-	// ks := keystore.NewKeyStore(".", keystore.StandardScryptN, keystore.StandardScryptP)
-	// newAcc, err := ks.ImportECDSA(privateKey, "")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// fmt.Println("Keystore path:", ks.JoinPath(newAcc.Address.Hex()))
-
-	// encode private key to hex string
-	hexPrivateKey := hex.EncodeToString(crypto.FromECDSA(privateKey))
-	fmt.Println("Hex private key:", hexPrivateKey)
-
-	// encode public key to hex string
-	hexPublicKey := hexutil.Encode(crypto.FromECDSAPub(publicKeyECDSA)[1:])
-	fmt.Println("Hex public key:", hexPublicKey)
-
-	fmt.Println("BIP32 path:", "m/44'/60'/0'/0/", index)
 }
